35ee0bdca983b855cdf9cc9b94c5b1c52710241a
sinaafshar13 <afshari.sina@outlook.com>
2024-03-05 14:14:07 +0330

    create react portfolio project(header-section)

---
+++ src/components/header/Header.js
@@ -0,0 +1,59 @@
+import "./Header.css";
+const Header = () => {
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="" className="nav-logo">
+          Afshar
+        </a>
+
+        <div className="nav-menu">
+          <ul className="nav-list">
+            <li className="nav-item">
+              <a href="#home" className="nav-link active-link">
+                <i className="bi bi-house-door-fill nav-icon"></i>
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#about" className="nav-link">
+                <i className="bi bi-person-vcard nav-icon"></i>
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#skills" className="nav-link">
+                <i className="bi bi-file-earmark-text nav-icon"></i>
+                Skills
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#services" className="nav-link">
+                <i className="bi bi-briefcase  nav-icon"></i>
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#portfolio" className="nav-link">
+                <i className="bi bi-image nav-icon"></i>
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#contact" className="nav-link">
+                <i className="bi bi-send nav-icon"></i>
+                Contact
+              </a>
+            </li>
+          </ul>
+          <i className="bi bi-x nav-close"></i>
+        </div>
+        <div className="nav-toggle">
+          <i className="bi bi-grid"></i>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,8 @@
+import Header from "../../components/header/Header";
+import './Home.css'
+function Home(){
+    return (
+        <Header />
+    )
+}
+export default Home;

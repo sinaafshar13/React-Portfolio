579db031b3851bd24322fb33531eae7e8149f368
sinaafshar13 <afshari.sina@outlook.com>
2026-06-23 03:23:10 +0330

    fix some build errors

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,14 +1,14 @@
-import About from "../../components/About/About";
-import Contact from "../../components/Contact/Contact";
-import Footer from "../../components/Footer/Footer";
-import Header from "../../components/Header/Header";
-import Home from "../../components/Home/Home";
-import Portfolio from "../../components/Portfolio/Portfolio";
-import Qualification from "../../components/Qualification/Qualification";
-import ScrollUp from "../../components/ScrollUp/ScrollUp";
-import Services from "../../components/Services/Services";
-import Skills from "../../components/Skills/Skills";
-import Testimonials from "../../components/Testimonials/Testimonials";
+import About from "../../components/About/About.js";
+import Contact from "../../components/Contact/Contact.js";
+import Footer from "../../components/Footer/Footer.js";
+import Header from "../../components/Header/Header.js";
+import Home from "../../components/Home/Home.js";
+import Portfolio from "../../components/Portfolio/Portfolio.js";
+import Qualification from "../../components/Qualification/Qualification.js";
+import ScrollUp from "../../components/ScrollUp/ScrollUp.js";
+import Services from "../../components/Services/Services.js";
+import Skills from "../../components/Skills/Skills.js";
+import Testimonials from "../../components/Testimonials/Testimonials.js";
 function LandingPage() {
   return (
     <>

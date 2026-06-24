142f1f9d34504f4b4a738f77cd05bfddf1d1eb13
sinaafshar13 <afshari.sina@outlook.com>
2026-06-24 16:18:26 +0330

    FIX header issue in landing page

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,6 +1,7 @@
 import About from "../../components/About/About.js";
 import Contact from "../../components/Contact/Contact.js";
 import Footer from "../../components/Footer/Footer.js";
+import Header from "../../components/Header/Header.js"
 import Home from "../../components/Home/Home.js";
 import Portfolio from "../../components/Portfolio/Portfolio.js";
 import Qualification from "../../components/Qualification/Qualification.js";
@@ -11,6 +12,7 @@ import Testimonials from "../../components/Testimonials/Testimonials.js";
 function LandingPage() {
   return (
     <>
+    <Header />
       <main className="main">
         <Home />
         <About />

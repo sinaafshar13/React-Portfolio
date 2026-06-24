a4e2889ec042ae70abad4595f02a2c77332b9fd6
sinaafshar13 <afshari.sina@outlook.com>
2026-06-24 14:33:12 +0330

    FIX header error

--- src/pages/LogInPage/LogInPage.js
+++ src/pages/LogInPage/LogInPage.js
@@ -1,12 +1,10 @@
 import Footer from "../../components/Footer/Footer.js";
-import Header from "../../components/Header/Header.js";
 import ScrollUp from "../../components/ScrollUp/ScrollUp.js";
 import LogIn from "../../components/LogIn/LogIn.js";
 
 function LogInPage() {
   return (
     <>
-      <Header />
       <main className="main">
         <LogIn />
       </main>
